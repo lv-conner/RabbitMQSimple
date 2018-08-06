f7155e2d19576f87286513466b5e6b401097c5a9
lv-conner <lvmh123456@gmail.com>
2018-08-06 15:05:38 +0800

    init rabbitmq learn code repository

---
+++ Client/Client/Client/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Util;
+using RabbitMQ.Client;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static IConnectionFactory factory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+        static void Main(string[] args)
+        {
+            //DefaultQueueComsumer();
+            //DirectQueueComsumer();
+            //Subscribe();
+            //TopicQueue();
+            HeaderQueue();
+            //using (var connection = factory.CreateConnection())
+            //using (var channel = connection.CreateModel())
+            //{
+            //    //channel.QueueDeclare("BroadCast", true, false, true, null);
+            //    var consumer = new EventingBasicConsumer(channel);
+            //    consumer.Received += Consumer_Received;
+            //    //var value = channel.BasicConsume("BroadCast", false, consumer);
+            //    channel.BasicConsume("DirectQueue", false, consumer);
+            //    Console.WriteLine("start listen");
+            //    Console.ReadKey();
+            //}
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var consumer = sender as IBasicConsumer;
+            consumer.Model.BasicAck(e.DeliveryTag, false);
+            Console.WriteLine("Recevied:\t" + Encoding.UTF8.GetString(e.Body));
+            Console.WriteLine(sender.GetType().FullName);
+        }
+
+        static void DefaultQueueComsumer()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+                channel.BasicConsume("DefaultQueue", false, consumer);
+                Console.ReadKey();
+            }
+        }
+
+        static void DirectQueueComsumer()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+                channel.BasicConsume("DirectQueue", false, consumer);
+                Console.ReadKey();
+            }
+        }
+
+
+        static void Subscribe()
+        {
+            Task.WaitAll(Task.Run(() => FanoutQueueComsumer()), Task.Run(() => FanoutQueueComsumer1()));
+        }
+        static void FanoutQueueComsumer()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+                channel.BasicConsume("FanoutQueue", false, consumer);
+                Console.ReadKey();
+            }
+        }
+
+        static void FanoutQueueComsumer1()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+                channel.BasicConsume("FanoutQueue1", false, consumer);
+                Console.ReadKey();
+            }
+        }
+
+
+
+
+        static void TopicQueue()
+        {
+            Task.WaitAll(Task.Run(() => GameQueue()), Task.Run(() => LiveQueue()), Task.Run(() => AllQueue()));
+        }
+        static void GameQueue()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+                channel.BasicConsume("GameTopicQueue", false, consumer);
+                Console.ReadKey();
+            }
+        }
+
+        static void LiveQueue()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+                channel.BasicConsume("LiveTopicQueue", false, consumer);
+                Console.ReadKey();
+            }
+        }
+
+        static void AllQueue()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+                channel.BasicConsume("AllTopicQueue", false, consumer);
+                Console.ReadKey();
+            }
+        }
+
+        static void HeaderQueue()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += Consumer_Received;
+                channel.BasicConsume("HeaderQueue", false, consumer);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ;
+using RabbitMQ.Util;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Server
+{
+    class Program
+    {
+        static IConnectionFactory factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            Port = 5672,
+            UserName = "admin",
+            Password = "admin"
+        };
+        static void Main(string[] args)
+        {
+            //DefaultExchange();
+            //DirectExchange();
+            //FanoutExchange();
+            //TopicExchange();
+            HeaderExchange();
+        }
+
+        /// <summary>
+        /// 默认交换机发布消息
+        /// </summary>
+        static void DefaultExchange()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("DefaultQueue", true, false, false, null);
+                channel.BasicPublish("", "DefaultQueue", null, Encoding.UTF8.GetBytes("Default Queue Message"));
+                Console.WriteLine("Default Queue Message publish complete");
+            }
+        }
+
+        /// <summary>
+        /// 直连交换机发布消息。适用于发布任务
+        /// </summary>
+        static void DirectExchange()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("DirectQueue", true, false, false, null);
+                channel.ExchangeDeclare("DirectQueue_Ex", "direct", true, false, null);
+                channel.QueueBind("DirectQueue", "DirectQueue_Ex", "DirectQueue", null);
+                channel.BasicPublish("DirectQueue_Ex", "DirectQueue", null, Encoding.UTF8.GetBytes("DirectQueue Message"));
+                Console.WriteLine("Direct Queue Message publish complete");
+            }
+        }
+
+        /// <summary>
+        /// 扇形交换机，用于发布订阅。
+        /// </summary>
+        static void FanoutExchange()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("FanoutQueue", true, false, false, null);
+                channel.QueueDeclare("FanoutQueue1", true, false, false, null);
+                channel.ExchangeDeclare("FanoutQueue_Ex", "fanout", true, false, null);
+                channel.QueueBind("FanoutQueue", "FanoutQueue_Ex", "DirectQueue", null);
+                channel.QueueBind("FanoutQueue1", "FanoutQueue_Ex", "DirectQueue", null);
+                channel.BasicPublish("FanoutQueue_Ex", "", null, Encoding.UTF8.GetBytes("FanoutQueue Message"));
+                Console.WriteLine("FanoutQueue Queue Message publish complete");
+            }
+        }
+
+        /// <summary>
+        /// 主题交换机，根据路由键分发给不同的队列。当路由键值为#时，与扇形交换机一致。
+        /// </summary>
+        static void TopicExchange()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("GameTopicQueue", true, false, false, null);
+                channel.QueueDeclare("LiveTopicQueue", true, false, false, null);
+                channel.QueueDeclare("AllTopicQueue", true, false, false, null);
+                channel.ExchangeDeclare("ApplicationExchange", "topic", true, false, null);
+                channel.QueueBind("GameTopicQueue", "ApplicationExchange", "Game", null);
+                channel.QueueBind("LiveTopicQueue", "ApplicationExchange", "Live", null);
+                channel.QueueBind("AllTopicQueue", "ApplicationExchange", "#", null);
+                channel.BasicPublish("ApplicationExchange", "Game", null, Encoding.UTF8.GetBytes("Game Message"));
+                channel.BasicPublish("ApplicationExchange", "Live", null, Encoding.UTF8.GetBytes("Live Message"));
+                Console.WriteLine("FanoutQueue Queue Message publish complete");
+
+            }
+        }
+
+        /// <summary>
+        /// 头交换机。根据消息头部进行投送消息。路由键值为Headers
+        /// </summary>
+        static void HeaderExchange()
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("HeaderQueue", true, false, false, null);
+                channel.ExchangeDeclare("HeaderQueue_Ex", "headers", true, false, null);
+                var prop = channel.CreateBasicProperties();
+                prop.Headers = new Dictionary<string, object>();
+                prop.Headers.Add("name", "tim");
+                channel.QueueBind("HeaderQueue", "HeaderQueue_Ex", "Game",prop.Headers);
+                channel.BasicPublish("HeaderQueue_Ex", "Game", prop, Encoding.UTF8.GetBytes("Headers Message"));
+                Console.WriteLine("HeaderQueue Queue Message publish complete");
+                Console.ReadKey();
+
+            }
+        }
+
+    }
+}
